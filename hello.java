2d476ab3996fbf1b2be0ee4c7a06c4a22cc2b49a
Tanuj Vishnoi <tanujkumarvishnoi@qainfotech.com>
2020-07-09 19:36:05 +0530

    Update hello.java
    added on line and tried to create conflicts

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 class hello{
 public static void main(String[] args)
      {
+System.out.println("!st sdsds");
 System.out.println("!st version");
 System.out.println("in dev branch");
 //I am on the server now and trying to replicate the same with the file to copy on locals
